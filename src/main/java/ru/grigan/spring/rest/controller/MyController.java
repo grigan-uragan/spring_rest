5f4580f76549a4b6fc996c25ad2b1340494ca60c
grigan-Uragan <prischepny@mail.ru>
2021-03-26 15:52:36 +0300

    PostMapping

--- src/main/java/ru/grigan/spring/rest/controller/MyController.java
+++ src/main/java/ru/grigan/spring/rest/controller/MyController.java
@@ -1,10 +1,7 @@
 package ru.grigan.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.grigan.spring.rest.entity.Employee;
 import ru.grigan.spring.rest.exception_handling.NoSuchEmployeeException;
 import ru.grigan.spring.rest.service.EmployeeService;
@@ -29,5 +26,11 @@ public class MyController {
                     + id + " in database");
         }
         return employeeById;
+    }
+
+    @PostMapping("/employee")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        service.saveEmployee(employee);
+        return employee;
     }
 }
