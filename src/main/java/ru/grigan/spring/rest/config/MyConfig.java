8e2a1dfb7ddb4efa7bba54ee3ce39a2e7d09a1a3
grigan-Uragan <prischepny@mail.ru>
2021-03-26 11:44:08 +0300

    project configuration

--- src/main/java/ru/grigan/spring/rest/config/MyConfig.java
+++ src/main/java/ru/grigan/spring/rest/config/MyConfig.java
@@ -1,4 +1,54 @@
 package ru.grigan.spring.rest.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "ru.grigan.spring.rest")
+@EnableWebMvc
+@EnableTransactionManagement
 public class MyConfig {
+
+    @Bean
+    public DataSource dataSourceBean() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/spring_db");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("password");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionfactoryBean() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSourceBean());
+        sessionFactoryBean.setPackagesToScan("ru.grigan.spring.rest.entity");
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        sessionFactoryBean.setHibernateProperties(properties);
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(localSessionfactoryBean().getObject());
+        return transactionManager;
+    }
 }
